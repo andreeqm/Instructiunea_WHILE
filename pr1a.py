c36ce25c4d8f55524f7bc5eb15bf2feb22c68aee
moscvinandreea <70642112+moscvinandreea@users.noreply.github.com>
2020-10-25 11:17:31 +0200

    Update pr1a.py

--- pr1a.py
+++ pr1a.py
@@ -1,3 +1,7 @@
+"""Se introduc succesiv numere nenule până la introducerea numărului 0. 
+Să se afişeze suma tuturor numerelor introduse. 
+Exemplu: Date de intrare   3  5  4  2  0  Date de ieşire 14."""
+
 nr=1
 suma=1
 while((nr>0)and(nr!=0)):
